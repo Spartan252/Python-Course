df8580fda3c1f4427edeca8e0fc45b06fbe6dd53
Spartan252 <santiagoeli.ag@gmail.com>
2025-07-16 21:40:04 -0600

    Day_19

--- a.py
+++ a.py
@@ -1,29 +1,14 @@
-sides = int(input("How many sides does the figure have? (1, 2, 3, etc.): "))
-degrees = 360 / sides
+sentence = "CodInG Is AwaSOMe"
 
-# Determine length based on number of sides
-if sides >= 191:
-    length = 2
-elif 99 <= sides <= 190:
-    length = 5
-elif 40 <= sides <= 98:
-    length = 10
-elif 20 <= sides <= 39:
-    length = 25
-elif 11 <= sides <= 19:
-    length = 50
-else:
-    length = 100
+#def reverse_sentence(sentence):
 
-import turtle
 
-timmy = turtle.Turtle()
-timmy.shape("turtle")
-timmy.color("chartreuse")
+words = sentence.split()
 
-for _ in range(sides):
-    timmy.forward(length)
-    timmy.right(degrees)
+print(words)
 
-my_screen = turtle.Screen()
-my_screen.exitonclick()
+words = words[::-1]
+print(words[::-1])
+
+print(" ".join(words))
+
